17f7c96fbd454ab57a92438a195cf60b9c993430
Juliopks013 <juliopks013@gmail.com>
2025-08-29 16:23:45 -0300

    implementando angular no Laravel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,25 +4,34 @@ import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
 import { Router } from '@angular/router';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+    usuario: any = {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
+    constructor(
+    public ApiService: ApiService
   ){ }
 
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  cadastrarUsuario() {
+    this.ApiService.post('usuario/registrar-se', this.usuario).subscribe(resp => {
+      console.log(resp);
+    })
   }
 
-}
+
+
+  }
+
+
